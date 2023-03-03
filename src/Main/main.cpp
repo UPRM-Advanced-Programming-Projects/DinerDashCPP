f14d0e96db3a83c780b8b5c6e32368c11041719c
Joel Andre <joel.alvarado2@upr.edu>
2023-03-03 15:16:29 -0400

    Window size is now static and cannot be resized

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -3,11 +3,19 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(820,768,OF_WINDOW);			// <-------- setup the GL context
+	// ofSetupOpenGL(820,768,OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofRunApp(new ofApp());
+	// ofRunApp(new ofApp());
+
+	
+	// Runs app in forced window size
+	ofGLFWWindowSettings settings;
+	settings.resizable = false;
+	settings.setSize(820, 768);
+	ofCreateWindow(settings);
+	return ofRunApp(new ofApp);
 
 }
